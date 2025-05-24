293c76d3c2f195fef4ddc606dba137898b80a261
mansjbo <mansjbo@aol.com>
2025-05-24 22:26:40 +0200

    fixing all errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Testing.....");
+	res.status(201).send("Testing.....");
 });
 
 app.get("/testing", (req, res) => {
