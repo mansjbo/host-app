ce51ec143cc0a4c69ef0a0a8f24d6a0f397e254f
mansjbo <mansjbo@aol.com>
2025-05-24 22:18:34 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.status("Testing.....");
+});
+
+app.get("/testing", (req, res) => {
+	res.status(201).json({ message: "Working Fine......" });
+});
+
+const PORT = 4496;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
