60b90282e06e885936bc0a1fc435d368efcb7aed
mansjbo <mansjbo@aol.com>
2025-05-24 22:24:59 +0200

    fixing status problem

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.status("Testing.....");
+	res.send("Testing.....");
 });
 
 app.get("/testing", (req, res) => {
